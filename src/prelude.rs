17a4be2894015618941ffffe16e7e0789b56d9f7
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2024-12-31 14:36:49 +1100

    chore: mod prelude - comment tracing contents

--- src/prelude.rs
+++ src/prelude.rs
@@ -10,19 +10,19 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[allow(dead_code)]
 pub struct W<T>(pub T);
 
-pub type TracingSubscriber = tracing_subscriber::fmt::SubscriberBuilder<
-    tracing_subscriber::fmt::format::DefaultFields,
-    tracing_subscriber::fmt::format::Format<tracing_subscriber::fmt::format::Full>,
-    // tracing_subscriber::EnvFilter,
->;
+// pub type TracingSubscriber = tracing_subscriber::fmt::SubscriberBuilder<
+//     tracing_subscriber::fmt::format::DefaultFields,
+//     tracing_subscriber::fmt::format::Format<tracing_subscriber::fmt::format::Full>,
+//     // tracing_subscriber::EnvFilter,
+// >;
 
-pub fn init_logger() -> TracingSubscriber {
-    tracing_subscriber::fmt()
-        .with_level(true)
-        .with_ansi(true)
-        .with_line_number(true)
-        .with_thread_ids(true)
-    // .with_env_filter(level)
-    // .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
-    // .with_timer(tracing_subscriber::fmt::time::SystemTime)
-}
+// pub fn init_logger() -> TracingSubscriber {
+//     tracing_subscriber::fmt()
+//         .with_level(true)
+//         .with_ansi(true)
+//         .with_line_number(true)
+//         .with_thread_ids(true)
+//     // .with_env_filter(level)
+//     // .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+//     // .with_timer(tracing_subscriber::fmt::time::SystemTime)
+// }
