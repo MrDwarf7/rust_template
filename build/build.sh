c27e5954c12373f9c0e031631771385f98582f67
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-01-21 00:00:57 +1100

    ci: Add updated workflows & buld script dep

---
+++ build/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+export ARTIFACT_NAME="csv_parser_rs-$1"
+
+# Build for the target
+cargo build --release --locked --target "$1"
+
+mkdir -p "$ARTIFACT_NAME"
+# Create the artifact
+cp "target/$1/release/csv_parser_rs" "$ARTIFACT_NAME"
+cp "README.md" "LICENSE-APACHE" "LICENSE-MIT" "$ARTIFACT_NAME"
+
+# Zip the artifact
+if ! command -v zip &> /dev/null
+then
+	sudo apt-get update && sudo apt-get install -yq zip
+fi
+zip -r "$ARTIFACT_NAME.zip" "$ARTIFACT_NAME"
