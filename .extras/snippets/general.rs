2b112311f3cedc8ff386122ceaa9bea4bcd1ef36
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2025-01-28 19:22:14 +1100

    feat: add substitute fn

--- .extras/snippets/general.rs
+++ .extras/snippets/general.rs
@@ -6,3 +6,11 @@ pub fn time<T>(t: &str, f: impl FnOnce() -> T) -> T {
     eprintln!("{t}: Elapsed: {:?}", elapsed);
     r
 }
+
+/// Substitute the `var` variable in a string with the given `val` value.
+///
+/// Variable format: `{{ var }}`
+fn substitute<'a: 'b, 'b>(str: &'a str, var: &str, val: &str) -> Cow<'b, str> {
+    let format = format!(r"\{{\{{[[:space:]]*{}[[:space:]]*\}}\}}", var);
+    Regex::new(&format).unwrap().replace_all(str, val)
+}
