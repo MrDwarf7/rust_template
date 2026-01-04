42cfbae71a47ea75cd6c8c5736c38128fad81f8b
MrDwarf7 <129040985+MrDwarf7@users.noreply.github.com>
2026-01-04 12:55:28 +1100

    feat: add Duration serialization module

--- .extras/snippets/general.rs
+++ .extras/snippets/general.rs
@@ -14,3 +14,25 @@ fn substitute<'a: 'b, 'b>(str: &'a str, var: &str, val: &str) -> Cow<'b, str> {
     let format = format!(r"\{{\{{[[:space:]]*{}[[:space:]]*\}}\}}", var);
     Regex::new(&format).unwrap().replace_all(str, val)
 }
+
+/// Serialize and deserialize `std::time::Duration` as human-readable seconds.
+mod human_duration {
+    use std::time::Duration;
+
+    use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+    pub fn serialize<S>(duration: &Duration, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        duration.as_secs().serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Duration, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let secs = u64::deserialize(deserializer)?;
+        Ok(Duration::from_secs(secs))
+    }
+}
